5a04c9f8e1ab517d8ee4e98acc5af82fde3b6e5a
noah kim <heanokim@gmail.com>
2023-02-22 00:50:30 +0900

    edited browserRouter basename

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
