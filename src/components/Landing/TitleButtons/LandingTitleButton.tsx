310ad284ca0c3b21a72810e7ed51d8bff427719b
noah kim <heanokim@gmail.com>
2023-02-22 14:00:06 +0900

    added button hover effects

--- src/components/Landing/TitleButtons/LandingTitleButton.tsx
+++ src/components/Landing/TitleButtons/LandingTitleButton.tsx
@@ -40,7 +40,7 @@ export const LandingTitleButton: React.FC<LandingTitlesProp> = ({
   } else {
     return (
       <button
-        className="title-button w-40 h-40 text-center border-2 border-white animate-slideInUp rounded-md"
+        className="title-button w-40 h-40 text-center border-2 border-white animate-slideInUp rounded-md hover:bg-cloud-blue-700 hover:border-cloud-blue-600"
         onClick={setDetails}
       >
         <div className="flex flex-col justify-center">
