edd83215425e4e283d13daa832a53de51cdaa3ce
noah kim <heanokim@gmail.com>
2023-03-29 00:45:05 +0700

    reordered db skill icons

--- src/components/SkillsSection/logos/databaseToolsLogos.tsx
+++ src/components/SkillsSection/logos/databaseToolsLogos.tsx
@@ -1,18 +1,16 @@
 import mongoDbLogo from "../../../assets/images/mongo-svgrepo-com.svg";
 import sqlServerLogo from "../../../assets/images/sql-server.webp";
 import mySqlLogo from "../../../assets/images/mysql-svgrepo-com.svg";
-// import herokuLogo from "../../../assets/images/heroku-icon-svgrepo-com.svg";
-// import railwayLogo from "../../../assets/images/railway-app.svg";
-// import githubLogo from "../../../assets/images/githublogo.svg";
 import postgresqlLogo from "../../../assets/images/postgresql-svgrepo-com.svg";
 import dockerLogo from "../../../assets/images/docker-svgrepo-com.svg";
 import redisLogo from "../../../assets/images/redis-svgrepo-com.svg";
 import turboRepoLogo from "../../../assets/images/turborepo.dark.svg";
+import nxLogo from "../../../assets/images/nx-logo.svg";
 import { ILogo } from "./languageLogos";
 
 const databaseToolsLogos: ILogo[] = [
   {
-    name: "Postgre SQL",
+    name: "PostgreSQL",
     src: postgresqlLogo,
     alt: "postgre sql logo",
   },
@@ -45,6 +43,11 @@ const databaseToolsLogos: ILogo[] = [
     name: "Turbo",
     src: turboRepoLogo,
     alt: "turbo logo",
+  },
+  {
+    name: "Nx",
+    src: nxLogo,
+    alt: "Nx logo",
   },
 ];
 
